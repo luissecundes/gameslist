f66d57d3702f9a6dd3f307955aee8544efa5611d
Luis <luissecundes@gmail.com>
2023-12-20 10:33:16 -0300

    Move games on list

--- src/main/java/com/personalproject/gameslist/services/GameListService.java
+++ src/main/java/com/personalproject/gameslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.personalproject.gameslist.dto.GameListDTO;
 import com.personalproject.gameslist.entities.GameList;
+import com.personalproject.gameslist.projection.GameMinProjection;
 import com.personalproject.gameslist.repositories.GameListRepository;
+import com.personalproject.gameslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -17,10 +19,28 @@ public class GameListService {
 		@Autowired
 		private GameListRepository gameListRepository;
 		
+		@Autowired
+		private GameRepository gameRepository;
+		
 		@Transactional(readOnly = true)
 		public List<GameListDTO> findAll() {
 			List<GameList> result = gameListRepository.findAll();
 			var dto = result.stream().map(x -> new GameListDTO(x)).toList();
 			return dto;
+		}
+		
+		@Transactional
+		public void move(Long listId, int sourceIndex, int destinationIndex) {
+			List<GameMinProjection> list = gameRepository.searchByList(listId);
+			GameMinProjection obj = list.remove(sourceIndex);
+			list.add(destinationIndex, obj);
+			
+			int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+			int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+			
+			for (int i = min; i <= max; i++) {
+				gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+			}
+
 		}
 }
